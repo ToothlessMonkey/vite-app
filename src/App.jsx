886f915d0318219bc537af535276b506a8642c81
ToothlessMonkey <rajdeep3000@gmail.com>
2022-10-18 12:29:17 +0530

    sub branch first commit
    added <p> and git text

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 
 import "./index.css";
 
 function App() {
-  return <p className=" bg-red-500 mx-auto my-52 text-5xl">hi</p>;
+  return (
+    <Fragment>
+      <p className=" bg-red-500 mx-auto my-52 text-5xl">hi</p>
+      <p>this is sub branch</p>
+    </Fragment>
+  );
 }
 
 export default App;
